db98a3712b0917bdbc6f2a41249b9197b904a1c8
devCynosure <sharanprakash.sfc@gmail.com>
2021-07-30 01:07:04 +0530

    Change program name.

--- array/arraySumMatch.java
+++ array/arraySumMatch.java
@@ -1,7 +1,7 @@
 import java.lang.*;
 import java.io.*;
 
-class prog1{
+class arraySumMatch{
   public static void main(String[] args) throws IOException{
 
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));

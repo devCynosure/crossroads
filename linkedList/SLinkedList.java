16ebf9a9f213436a594b9156d91e4c1d77fa3e00
devCynosure <sharanprakash.sfc@gmail.com>
2021-07-31 01:15:57 +0530

    Finish linkedlist(add elements, display elements)

--- linkedList/SLinkedList.java
+++ linkedList/SLinkedList.java
@@ -1,13 +1,46 @@
-public class SLinkedList{
-  class Node{
-    int data;
-    Node next;
+public class SLinkedList {
+public Node head = null;
+public Node tail = null;
+class Node {
+int data;
+Node next;
 
-    Node(int data){
-      this.data = data;
-    }
-  }
+Node(int data){
+        this.data = data;
+}
+}
 
-  public Node head = null;
-  public Node tail = null;
+public void addElement(int data){
+        Node newNode = new Node(data);
+        if(head == null) {
+                head = newNode;
+        }else{
+                tail.next = newNode;
+        }
+        tail = newNode;
+}
+
+public void show(){
+
+        if(head == null) {
+                System.out.println("List is Empty");
+                return;
+        }
+        Node temp = head;
+        while(temp!= null) {
+                System.out.println("Data : " + temp.data);
+                temp = temp.next;
+        }
+}
+
+public static void main(String[] args){
+        SLinkedList sll = new SLinkedList();
+        sll.show();
+        sll.addElement(10);
+        sll.addElement(7);
+        sll.addElement(40);
+        sll.show();
+
+}
+
 }

5b3953ffaab56c2bb6d631ec1bc908254e0598f1
devCynosure <sharanprakash.sfc@gmail.com>
2021-07-30 21:31:59 +0530

    Work on linkedList.

---
+++ linkedList/SLinkedList.java
@@ -0,0 +1,13 @@
+public class SLinkedList{
+  class Node{
+    int data;
+    Node next;
+
+    Node(int data){
+      this.data = data;
+    }
+  }
+
+  public Node head = null;
+  public Node tail = null;
+}

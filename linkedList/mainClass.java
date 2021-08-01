08429c562b1f629a21b623369b8a043600fb61b0
devCynosure <sharanprakash.sfc@gmail.com>
2021-08-02 03:40:38 +0530

    Make single linkedlist own delete logic and do test case check.

--- linkedList/mainClass.java
+++ linkedList/mainClass.java
@@ -38,7 +38,46 @@ public void display(){
         }
 }
 
+public void deleteNode(int data){
+  System.out.println("Item to be deleted is : "+data);
+    System.out.println("Head is : "+head.data +" Tail is : "+tail.data);
+        if(head == null) {
+                System.out.println("List is Empty..");
+                return;
+        }
+        Node temp = head;
+        Node prev = null;
+        System.out.println("node not empty in deleteNode. " + temp.data);
+        if(temp.data == data) {
+          System.out.println("in head.");
+                head = head.next;
+                return;
+        }
+        prev = temp;
+        temp = head.next;
+        while(temp != null && temp.data != data) {
+                prev = temp;
+                temp = temp.next;
+        }
+        if(temp == null) {
+                System.out.println("Item is not present in list");
+                return;
+        }
+        else
+        if(tail.data == data) {
+          System.out.println("in tail. prev, tail " + prev + " "+ prev.data+" " + tail + " " + tail.data);
+          System.out.println("in tail, tail.data  last " + data +" " + tail.data);
+                tail = prev;
+                tail.next = null;
+                System.out.println("Tail exit. prev, tail " + prev + " "+ prev.data+" " + tail + " " + tail.data);
+        }
+        else{
+          System.out.println("in Middle " + prev.next +" " + temp.next);
+            prev.next = temp.next;
+            System.out.println("Middle End. head, tail " + head +" " + tail);
 
+        }
+}
 }
 
 class mainClass {
@@ -46,11 +85,11 @@ public static void main(String[] args){
         int i=0;
         Random random = new Random();
         SLinkedList sl = new SLinkedList();
-        while(i < 100) {
-                sl.addNode(random.nextInt(150));
+        while(i < 4) {
+                sl.addNode(random.nextInt(4));
+                sl.display();
+                sl.deleteNode(random.nextInt(4));
                 i++;
         }
-        sl.display();
-
 }
 }

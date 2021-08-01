a3529913ae083f9a8af4abbe2cd3d4ae28443d51
devCynosure <sharanprakash.sfc@gmail.com>
2021-08-02 01:05:10 +0530

    Finish SingleLinkedList addNode, display functions.

--- linkedList/SLinkedList.java
+++ linkedList/SLinkedList.java
@@ -1,72 +1,50 @@
-public class SLinkedList {
+public class SLinkedList{
+
 public Node head = null;
 public Node tail = null;
-class Node {
-int data;
-Node next;
 
-Node(int data){
-        this.data = data;
+class Node{
+  int data;
+  Node next;
+
+  Node(int data){
+    this.data = data;
+  }
+}
+
+public addNode(int data){
+  Node newNode = new Node(data);
+    if(head == null){
+      head = newNode;
+    }else{
+      tail.next = newNode;
+    }
+    tail = newNode;
+    System.out.println("New node inserted ..");
+}
+
+public display(){
+  Node temp;
+  if(head == null){
+    System.out.println("List is empty.");
+    return;
+  }
+  temp = head;
+  while(temp!=null){
+    System.out.println("Element is : " + temp.data){
+      
+    }
+  }
 }
 }
 
-public void addElement(int data){
-        Node newNode = new Node(data);
-        if(head == null) {
-                head = newNode;
-        }else{
-                tail.next = newNode;
-        }
-        tail = newNode;
-}
-
-public void show(){
-
-        if(head == null) {
-                System.out.println("List is Empty");
-                return;
-        }
-        Node temp = head;
-        while(temp!= null) {
-                System.out.println("Data : " + temp.data);
-                temp = temp.next;
-        }
-}
-
-public void remove(int data){
-        Node temp = head;
-        Node prev = head;
-        if(temp !=null && temp.data == data) {
-                head = temp.next;
-                return;
-        }
-        while(temp!= null && temp.data != data) {
-                prev = temp;
-                temp = temp.next;
-        }
-        if(temp == null) {
-                System.out.println("List is empty");
-                return;
-        }
-        if(temp == tail) {
-                tail = prev;
-                tail.next = null;
-                return;
-        }
-        prev.next = temp.next;
-
-}
-
-public static void main(String[] args){
-        SLinkedList sll = new SLinkedList();
-        sll.show();
-        sll.addElement(10);
-        sll.addElement(7);
-        sll.addElement(40);
-        sll.show();
-        sll.remove(10);
-        sll.show();
-
-}
-
+public class mainClass{
+  public static void main(String[] args){
+    int i=0;
+    SlinkedList sl = new SlinkedList();
+    while(i < 100){
+    sl.addNode(i);
+    i++;
+  }
+  }
 }
